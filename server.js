5be6ab46267d3510275aec5ed9ab570c314e2455
gamerfoxx <admin@josephludbrook.com>
2023-08-29 14:47:31 -0400

    set up morgan, and .env file

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-console.log('server is working');
+import express from 'express';
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-import { valueSecond } from './test-named.js';
-import value from './test-default.js';
+const app = express();
 
-console.log(value);
-console.log(valueSecond);
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Hello world');
+});
+
+app.post('/', (req, res) => {
+	console.log(req);
+	res.json({ message: 'res received', data: req.body });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+	console.log(`server running on port ${port}`);
+});
--- test-named.js
+++
@@ -1,1 +0,0 @@
-export const valueSecond = 42;
