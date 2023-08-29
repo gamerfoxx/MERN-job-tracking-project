334d105e97df75a31a8ed8d8a9cdb59225f5cf95
gamerfoxx <admin@josephludbrook.com>
2023-08-29 15:13:50 -0400

    set up edit job

--- server.js
+++ server.js
@@ -57,6 +57,24 @@ app.get('/api/v1/jobs/:id', (req, res) => {
 	res.status(200).json({ job });
 });
 
+//edit job
+app.patch('/api/v1/jobs/:id', (req, res) => {
+	const { company, position } = req.body;
+	if (!company || !position) {
+		res.status(400).json({ msg: 'Please provide valid company and position' });
+		return;
+	}
+	const { id } = req.params;
+	const job = jobs.find((job) => job.id === id);
+	if (!job) {
+		res.status(404).json({ msg: 'Please provide valid ID' });
+		return;
+	}
+	job.company = company;
+	job.position = position;
+	res.status(200).json({ msg: 'job modified', job });
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
