5841ccd4e5f3ffaf144ad18f1cc73f401bbf4628
gamerfoxx <admin@josephludbrook.com>
2023-09-02 11:19:16 -0400

    force only first user to be admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@ import { StatusCodes } from 'http-status-codes';
 import UserModel from '../models/UserModel.js';
 
 export const register = async (req, res) => {
+	const firstAccount = (await UserModel.countDocuments()) === 0;
+	req.body.role = firstAccount ? 'admin' : 'user';
 	const user = await UserModel.create(req.body);
 	res.status(StatusCodes.CREATED).json({ user });
 };
