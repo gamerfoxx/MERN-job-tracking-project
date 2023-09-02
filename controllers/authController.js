c46ed3b9f7c6f0a9511af45b7acc1cf13ff5bafb
gamerfoxx <admin@josephludbrook.com>
2023-09-02 11:27:28 -0400

    create password hash for security

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,15 @@
 import { StatusCodes } from 'http-status-codes';
 import UserModel from '../models/UserModel.js';
+import { hashPassword } from '../utils/passUtils.js';
 
 export const register = async (req, res) => {
 	const firstAccount = (await UserModel.countDocuments()) === 0;
 	req.body.role = firstAccount ? 'admin' : 'user';
+	const hashedPass = await hashPassword(req.body.password);
+	req.body.password = hashedPass;
+
 	const user = await UserModel.create(req.body);
-	res.status(StatusCodes.CREATED).json({ user });
+	res.status(StatusCodes.CREATED).json({ msg: 'User Created' });
 };
 export const login = async (req, res) => {
 	res.send('login');
