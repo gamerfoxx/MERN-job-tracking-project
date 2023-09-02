11cb4b0c61e4a5b3b6a20a43112be96deb2de93d
gamerfoxx <admin@josephludbrook.com>
2023-09-02 10:56:46 -0400

    create user model

---
+++ models/UserModel.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+	name: String,
+	email: String,
+	password: String,
+	lastName: { type: String, default: 'lastName' },
+	location: { type: String, default: 'location' },
+	role: { type: String, enum: ['user', 'admin'], default: 'user' },
+});
+
+export default mongoose.model('User', UserSchema);
