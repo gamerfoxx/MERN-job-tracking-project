408a81f157687a0419744ae5767d185ddc6f48a6
gamerfoxx <admin@josephludbrook.com>
2023-09-02 12:47:55 -0400

    continue authentication

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,8 @@
+import { UnauthenticatedError } from '../errors/customErrors.js';
+
 export const authenticateUser = async (req, res, next) => {
+	const { token } = req.cookies;
+	if (!token) throw new UnauthenticatedError('authentication invalid');
 	console.log(req.cookies);
 	next();
 };
