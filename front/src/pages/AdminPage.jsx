8cab8d522a773690a1999d514e83c8dbc5f9af56
gamerfoxx <admin@josephludbrook.com>
2023-09-04 15:45:30 -0400

    complete admin page

--- front/src/pages/AdminPage.jsx
+++ front/src/pages/AdminPage.jsx
@@ -1,4 +1,40 @@
+import { FaSuitcaseRolling, FaCalendarCheck } from 'react-icons/fa';
+
+import { useLoaderData, redirect } from 'react-router-dom';
+import customFetch from '../utils/customFetch';
+import Wrapper from '../assets/wrappers/StatsContainer';
+import { toast } from 'react-toastify';
+import StatItem from '../components/StatItem';
+
+export const loader = async () => {
+	try {
+		const response = await customFetch.get('user/admin/appstats');
+		return response.data;
+	} catch (err) {
+		toast.error('Not authorized ');
+		return redirect('/dashboard');
+	}
+};
+
 const AdminPage = () => {
-	return <h1>AdminPage</h1>;
+	const { users, jobs } = useLoaderData();
+	return (
+		<Wrapper>
+			<StatItem
+				title="Current users"
+				count={users}
+				color="red"
+				bcg="blue"
+				icon={<FaSuitcaseRolling />}
+			/>
+			<StatItem
+				title="total jobs"
+				count={jobs}
+				color="red"
+				bcg="blue"
+				icon={<FaCalendarCheck />}
+			/>
+		</Wrapper>
+	);
 };
 export default AdminPage;
