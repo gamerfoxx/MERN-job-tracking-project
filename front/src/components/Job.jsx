6d95be0de1bb822c384c139924b9a04d7a70de48
gamerfoxx <admin@josephludbrook.com>
2023-09-04 13:27:08 -0400

    add new components, and get job partially functionaly

--- front/src/components/Job.jsx
+++ front/src/components/Job.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Form, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Job';
 import JobInfo from './JobInfo';
 import day from 'dayjs';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
+import CustomButton from './CustomButton';
 day.extend(advancedFormat);
 
 const Job = ({
@@ -25,7 +26,45 @@ const Job = ({
 		createdAt,
 		jobStatus
 	);
-	return <div>Job</div>;
+	const date = day(createdAt).format('MMM dd, YYYY ');
+	return (
+		<Wrapper>
+			<header>
+				<div className="main-icon">{company.charAt(0)}</div>
+				<div className="info">
+					<h5>{position}</h5>
+					<p>{company}</p>
+				</div>
+			</header>
+			<div className="content">
+				<div className="content-center">
+					<JobInfo
+						icon={<FaLocationArrow />}
+						text={jobLocation}
+					/>
+					<JobInfo
+						icon={<FaCalendarAlt />}
+						text={date}
+					/>
+					<JobInfo
+						icon={<FaBriefcase />}
+						text={jobType}
+					/>
+					<div className={`status ${jobStatus}`}>{jobStatus}</div>
+					<footer className="actions">
+						<Link className="btn edit-btn">Edit</Link>
+						<Form>
+							<CustomButton
+								type="submit"
+								classes="delete-btn"
+								label="Delete"
+							/>
+						</Form>
+					</footer>
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
 
 export default Job;
