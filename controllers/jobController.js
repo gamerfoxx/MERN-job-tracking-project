d740a08c288469ccf7213008c16a91a2ea72d77a
gamerfoxx <admin@josephludbrook.com>
2023-09-04 18:23:12 -0400

    add populate for base stats data

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,7 @@
 import 'express-async-errors';
 import { StatusCodes } from 'http-status-codes';
+import mongoose from 'mongoose';
+import day from 'dayjs';
 
 import JobModel from '../models/JobModel.js';
 
@@ -34,4 +36,49 @@ export const deleteJob = async (req, res) => {
 	const removedJob = await JobModel.findByIdAndDelete(req.params.id);
 
 	res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removedJob });
+};
+
+export const showStats = async (req, res) => {
+	let stats = await JobModel.aggregate([
+		{ $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+		{ $group: { _id: '$jobStatus', count: { $sum: 1 } } },
+	]);
+
+	stats = stats.reduce((acc, curr) => {
+		const { _id: title, count } = curr;
+		acc[title] = count;
+		return acc;
+	}, {});
+
+	const defaultStats = {
+		pending: stats.pending || 0,
+		interview: stats.interview || 0,
+		declined: stats.declined || 0,
+	};
+	let monthlyApps = await JobModel.aggregate([
+		{ $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+		{
+			$group: {
+				_id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
+				count: { $sum: 1 },
+			},
+		},
+		{ $sort: { '_id.year': -1, '_id.month': -1 } },
+		{ $limit: 6 },
+	]);
+
+	monthlyApps
+		.map((item) => {
+			const {
+				_id: { year, month },
+				count,
+			} = item;
+			const date = day()
+				.month(month - 1)
+				.year(year)
+				.format('MMM YY');
+			return { date, count };
+		})
+		.reverse();
+	res.status(StatusCodes.OK).json({ defaultStats, monthlyApps });
 };
