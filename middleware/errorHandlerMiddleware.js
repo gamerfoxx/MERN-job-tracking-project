d985f977b3ce54d32af898362a5a5b7d0b10ca7a
gamerfoxx <admin@josephludbrook.com>
2023-09-05 21:10:57 -0400

    console logs

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 const errorHandlerMiddleware = (err, req, res, next) => {
 	const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
 	const msg = err.message || 'Something is not working';
+	console.log(err);
 	res.status(statusCode).json({ msg });
 };
 
